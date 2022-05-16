9f399034cf87bcfb1ee9029d287e19bb7870f035
isaac_lee <yy0221ss@gmail.com>
2022-05-16 12:51:04 +0900

    Patterns

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,11 @@
 import { AppProps } from 'next/dist/shared/lib/router/router'
-import NavBar from '../components/NavBar'
+import Layout from '../components/Layout'
 import "../styles/globals.css"
 
 export default function App({Component, pagesProps}: AppProps) {
   return (
-    <div>
-      <NavBar />
+    <Layout>
       <Component {...pagesProps} />
-      <style jsx global>{`
-        a {
-          color: white;
-        }
-      `}</style>
-    </div>
+    </Layout>
   )
 }
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,9 @@
-import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
 
 export default function About() {
   return (
     <div>
+      <Seo title="About" />
       <h1>About me...</h1>
     </div>
   );
